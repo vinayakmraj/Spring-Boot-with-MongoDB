cb3adbbf6bbac92bca862e3c24d3a97abee456cf
vinayakmraj <vinayak.mraj@gmail.com>
2025-11-21 06:58:33 +0530

    pom.xml Updated with Project Lombok

--- src/main/java/com/example/demo/entity/JournalEntry.java
+++ src/main/java/com/example/demo/entity/JournalEntry.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,6 +9,8 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 import java.util.Date;
 
+@Setter
+@Getter
 @Document(collection = "journal_Entries")
 public class JournalEntry {
     @Id
@@ -15,35 +19,4 @@ public class JournalEntry {
     private String content;
     private LocalDateTime date;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
